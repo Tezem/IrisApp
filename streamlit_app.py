639d7f527388221bdced6a8f65ddc2e21084cab2
Tezem <tezemfredo@gmail.com>
2025-02-12 20:36:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import pandas as pd
+# Charger le fichier CSV
+file_path = "iris.csv"  # Assurez-vous que le fichier est dans le bon dossier
+df = pd.read_csv(file_path)
 
+# Afficher les 5 premières lignes
+print(df.head())
+
+# Informations générales sur les données
+print(df.info())
+
+# Statistiques descriptives
+print(df.describe())
+
+# Vérification des valeurs manquantes
+print(df.isnull().sum())
 st.write("Hello world !")
