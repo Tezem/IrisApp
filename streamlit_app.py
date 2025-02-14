a4d8e4c1f00ca0cbe98b82641ab5403bfba9af96
Tezem <tezemfredo@gmail.com>
2025-02-14 10:56:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from sklearn.model_selection import train_test_split
 
 st.write("Hello world !")
+
+# Séparer les caractéristiques et la cible
+X = df.drop('species', axis=1)
+y = df['species']
