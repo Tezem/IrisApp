cd97393bd7be00731b768a259427604fbbee9ca1
Tezem <tezemfredo@gmail.com>
2025-02-14 10:34:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,3 @@
 import streamlit as st
-import pandas as pd
-import os
 
-# Définir le chemin correct
-file_path = os.path.join(os.path.dirname(__file__), "data", "iris.csv")
-
-# Vérifier si le fichier existe
-if not os.path.exists(file_path):
-    raise FileNotFoundError(f"Fichier introuvable : {file_path}")
-
-# Charger les données
-df = pd.read_csv(file_path)
-print(df.head())
 st.write("Hello world !")
