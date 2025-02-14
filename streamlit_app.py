ca4d32d965f177720e3afbacde04cdc79ac128b0
Tezem <tezemfredo@gmail.com>
2025-02-14 11:14:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 #from sklearn.model_selection import train_test_split
 
 st.write("Hello world !")
+st.line_chart(df)
 # Séparer les caractéristiques et la cible
 #X = df.drop('species', axis=1)
 #y = df['species']
